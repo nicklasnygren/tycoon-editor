7aed9d4290d9c537829a53014d264660cf1f1d2c
Nicklas Nygren <hello@nickl.as>
2014-01-26 21:44:57 +0100

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('techtreeBuilderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
